b39e87afb86192ce0b4ce9d4d5adfec8afddd73c
zuev <pavelzuevwork@gmail.com>
2025-09-21 21:49:33 +0300

    skelet

---
+++ backend/showcase-backend/src/test/kotlin/ru/miit/showcase/backend/ShowcaseBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.miit.showcase.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShowcaseBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
